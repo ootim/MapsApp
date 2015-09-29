e83cdde64a5d4c00583f2fd390c0e126a77e1a0d
Tim <o.oladoyinbo@gmail.com>
2015-09-29 15:21:52 +0100

    Just added a comment

---
+++ src/com/example/MapsApp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.MapsApp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //comment 
+        setContentView(R.layout.main);
+    }
+}
